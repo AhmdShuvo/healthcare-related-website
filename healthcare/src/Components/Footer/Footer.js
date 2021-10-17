689e5ce9fea14ba3247ce7e6814780938226c59f
Shuvo <74643721+AhmdShuvo@users.noreply.github.com>
2021-10-17 23:25:53 +0600

    json

---
+++ healthcare/src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+             <center> <h3>Footer</h3></center>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ healthcare/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h3>Headder</h3>
+        </div>
+    );
+};
+
+export default Header;<h3>Headder</h3>
